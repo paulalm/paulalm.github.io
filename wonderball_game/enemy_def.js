054857278b87644ee97d50ca3366c7c81b5e25b5
unknown <80556072+bellanochepijamas@users.noreply.github.com>
2022-07-10 15:31:03 -0400

    shovel added

--- wonderball_game/enemy_def.js
+++ wonderball_game/enemy_def.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 enemyImages = []
 enemyTypes = []
 
@@ -40,3 +41,47 @@ const enemyType2 = {
   framesAttack: 1
 }
 enemyTypes.push(enemyType2);
+=======
+enemyImages = []
+enemyTypes = []
+
+const enemy1 = new Image();
+enemy1.src = 'enemies/enemy1.png';
+enemyImages.push(enemy1);
+
+const enemyType0 = {
+  img: enemyImages[0],
+  maxHealth: 100,
+  speedFactor: 0.2,
+  frameAttack: 3,
+  frameBadHealth: 4,
+  framesStart: 3,
+  framesBadHealth: 2,
+  framesAttack: 1
+}
+enemyTypes.push(enemyType0);
+
+const enemyType1 = {
+  img: enemyImages[0],
+  maxHealth: 200,
+  speedFactor: 0.25,
+  frameAttack: 3,
+  frameBadHealth: 4,
+  framesStart: 3,
+  framesBadHealth: 2,
+  framesAttack: 1
+}
+enemyTypes.push(enemyType1);
+
+const enemyType2 = {
+  img: enemyImages[0],
+  maxHealth: 1000,
+  speedFactor: 0.2,
+  frameAttack: 3,
+  frameBadHealth: 4,
+  framesStart: 3,
+  framesBadHealth: 2,
+  framesAttack: 1
+}
+enemyTypes.push(enemyType2);
+>>>>>>> Stashed changes
