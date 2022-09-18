b79aa25a2e609fb5df18d8be19d626ee7ee27976
unknown <80556072+bellanochepijamas@users.noreply.github.com>
2022-09-18 16:41:59 -0400

    boss image

--- wonderballgame/enemy_def.js
+++ wonderballgame/enemy_def.js
@@ -5,6 +5,10 @@ const enemy1 = new Image();
 enemy1.src = 'enemies/enemy1.png';
 enemyImages.push(enemy1);
 
+const enemy2 = new Image();
+enemy2.src = 'enemies/boss.png';
+enemyImages.push(enemy2);
+
 const enemyType0 = {
   img: enemyImages[0],
   maxHealth: 100,
@@ -30,13 +34,13 @@ const enemyType1 = {
 enemyTypes.push(enemyType1);
 
 const enemyType2 = {
-  img: enemyImages[0],
+  img: enemyImages[1],
   maxHealth: 1000,
   speedFactor: 0.2,
-  frameAttack: 3,
-  frameBadHealth: 4,
-  framesStart: 3,
-  framesBadHealth: 2,
+  frameAttack: 1,
+  frameBadHealth: 1,
+  framesStart: 1,
+  framesBadHealth: 1,
   framesAttack: 1
 }
 enemyTypes.push(enemyType2);
